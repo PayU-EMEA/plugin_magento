fa206a090425e5bde7f8db4ec6df741d51fd2b38
agnagna <agnieszka.wisniewska@payu.pl>
2014-08-25 10:33:18 +0200

    Plugin version updated

--- app/code/community/PayU/Account/Model/Config.php
+++ app/code/community/PayU/Account/Model/Config.php
@@ -14,7 +14,7 @@
 class PayU_Account_Model_Config
 {
 	/** @var string self version */
-	protected $_pluginVersion = '1.9.0';
+	protected $_pluginVersion = '2.0.2';
 	
 	/** @var string minimum Magento e-commerce version */
 	protected $_minimumMageVersion = '1.6.0';
